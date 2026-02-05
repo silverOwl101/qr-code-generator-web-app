3ae0a7f86c129e3f18200e435151bd52263006f6
silverOwl <contact.palucajh@gmail.com>
2026-02-05 23:58:31 +0800

    fix: adjust layout and enhance QR code download functionality in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,10 +7,14 @@
                 </div>
             </template>
             <div class="flex flex-col gap-6 py-4">
-                <UInput v-model="qrInput" placeholder="Enter text to generate QR code" size="lg" class="w-full"
+                <UInput
+v-model="qrInput" placeholder="Enter text to generate QR code" size="lg" class="w-full"
                     icon="i-heroicons-pencil-square" autofocus />
-                <div class="flex justify-center">
+                <div class="flex justify-center qrcode-wrapper">
                     <Qrcode :value="qrInput" class="w-48 h-48 rounded-lg border border-gray-200" />
+                </div>
+                <div class="flex justify-center">
+                    <UButton color="primary" @click="downloadQR">Download</UButton>
                 </div>
             </div>
             <template #footer>
@@ -22,5 +26,22 @@
 </template>
 
 <script lang="ts" setup>
+
 const qrInput = ref('')
+
+const downloadQR = () => {    
+    const svg = document.querySelector('.qrcode-wrapper svg')
+    if (!svg) return
+    const serializer = new XMLSerializer()
+    const source = serializer.serializeToString(svg)
+    const blob = new Blob([source], { type: 'image/svg+xml;charset=utf-8' })
+    const url = URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.href = url
+    link.download = 'qrcode.svg'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    URL.revokeObjectURL(url)
+}
 </script>
